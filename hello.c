cb9f43ebed8ef2040d3b8cf82936253dfcc3b3a7
Cristian Ruiz Garduno <cruizgarduno2@huskers.unl.edu>
2022-01-28 15:54:06 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Cristian Ruiz Garduno, Miguel Ortic
+ *         cbourke@cse.unl.edu
+ * Date: 2022/28/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cristian Ruiz Garduno!\n");
+  printf("Miguel Ortic\n");
+
+  return 0;
+}
+
